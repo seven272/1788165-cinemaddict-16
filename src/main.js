f4ec8a068ca573822860ec7607df3a8c5ac0da12
Mikhail <i727i@mail.ru>
2021-11-25 23:51:44 +0300

    1.9. Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import {createSiteMenuTemplate} from './view/site-menu-view.js';
+import {createButtonShowMoreTemplate} from './view/button-show-more-view.js';
+import {createTitleUserTemplate} from './view/title-user-view.js';
+import {createStatisticFilmTemplate} from './view/statistic-film-view.js';
+import {createPopupFilmTemplate} from './view/popup-view.js';
+import {createCardFilmTemplate} from './view/card-film-list-view.js';
+
 import {renderTemplate, RenderPosition} from './render.js';
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const CARD_FILM_COUNT = 5;
 
-renderTemplate(siteHeaderElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = document.querySelector('.header');
+const siteFooterElement = document.querySelector('.footer');
+const siteFooterStatisticElement = siteFooterElement.querySelector('.footer__statistics');
+
+const siteFilmListContainerElement = document.querySelector('.films-list__container')
+
+renderTemplate(siteHeaderElement, createTitleUserTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+for(let i = 0; i < CARD_FILM_COUNT;i++) {
+ renderTemplate(siteMainElement, createCardFilmTemplate(), RenderPosition.BEFOREEND);
+}
+renderTemplate(siteMainElement, createButtonShowMoreTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteFooterStatisticElement, createStatisticFilmTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createPopupFilmTemplate(), RenderPosition.BEFOREEND);
